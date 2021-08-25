26efe8458f144c4fefef1d64f64e9224ff17f28f
Kugi Haito <kugihaito99@gmail.com>
2021-08-25 16:18:32 -0300

    Change folder `interfaces` to `enums`

---
+++ CSSObject/enums/ICSSStatments.js
@@ -0,0 +1,21 @@
+/**
+ * @enum ICSSStatments
+ * CSS Staments Names
+ */
+const ICSSStatments = {
+
+	/** Import CSS Statment */
+	IMPORT: 'import',
+	
+	/** Media Query CSS Statment */
+	MEDIA: 'media',
+	
+	/** Keyframe CSS Statment */
+	KEYFRAME: 'keyframe',
+	
+	/** Font Face CSS Statment */
+	FONT_FACE: 'font-face'
+
+}
+
+export default ICSSStatments
