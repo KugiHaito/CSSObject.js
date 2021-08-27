26cda495dcd1a10e742d469b3f56d9c2ce78fc47
Kugi Haito <kugihaito99@gmail.com>
2021-08-27 01:12:55 -0300

    Add ICombinator enum

---
+++ CSSObject/enums/ICombinator.js
@@ -0,0 +1,22 @@
+/**
+ * @enum ICombinator
+ * Selector combinators
+ */
+const ICombinator = {
+
+	/** Combinator Descendant Selector */
+	DESCENDANT_SELECTOR: " ",
+
+	/** Combinator Adjacent Sibling Selector */
+	ADJACENT_SIBLING: "+",
+	
+	/** Combinator General Sibling Selector */
+	GENERAL_SIBLING: "~",
+	
+	/** Combinator Children Selector */
+	CHILDREN_SELECTOR: ">",
+
+}
+
+
+export default ICombinator
