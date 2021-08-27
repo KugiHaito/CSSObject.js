b14639ebc226875a58c4e20b7289a571ec5f5254
Kugi Haito <kugihaito99@gmail.com>
2021-08-26 21:42:57 -0300

    Add IPseudo enum

---
+++ CSSObject/enums/IPseudo.js
@@ -0,0 +1,19 @@
+/**
+ * @enum IPseudo
+ * Pseudo Types
+ */
+const IPseudo = {
+
+	/** Pseudo Class Type Index */
+	PseudoClass: ':',
+
+	/** Pseudo Element Type Index */
+	PseudoElement: '::',
+
+	/** Pseudo Event Type Index */
+	PseudoEvent: '@',
+
+}
+
+
+export default IPseudo
