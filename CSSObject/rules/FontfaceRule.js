649a79d4eed23086736de18835f95fb7f10b8993
Kugi Haito <kugihaito99@gmail.com>
2021-08-30 21:40:31 -0300

    Update FontfaceRule

--- CSSObject/rules/FontfaceRule.js
+++ CSSObject/rules/FontfaceRule.js
@@ -1,18 +1,31 @@
+import BlocksParser from "../parser/BlocksParser.js"
+import ParserBlock from "../parser/ParserBlock.js"
+import ICSS from "../enums/ICSS.js"
+
+
 /**
  * Class FontfaceRule
  * @class FontfaceRule
  */
-class FontfaceRule {
+class FontfaceRule extends BlocksParser(ParserBlock) {
 
 	/**
 	 * Initialize FontfaceRule
-	 * @param {string} rules
+	 * @param {string} fontface
 	 * @returns {FontfaceRule}
 	 */
-	constructor(rules) {
-		this.rules = rules
+	constructor(fontface) {
+		super()
 
+		this.fontface = fontface
 		return this
+	}
+
+	set fontface(values) {
+		this.rules = this.rules(values)
+		let r = this.rules
+
+		this.name = r.find(r => r.prop == 'font-family').value.join('')
 	}
 
 }
