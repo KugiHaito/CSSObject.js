16c97319f4a38f02b91905f3587219397d36966a
Kugi Haito <kugihaito99@gmail.com>
2021-08-30 22:21:45 -0300

    Update KeyframeRule class

--- CSSObject/rules/KeyframeRule.js
+++ CSSObject/rules/KeyframeRule.js
@@ -1,19 +1,31 @@
+import BlocksParser from "../parser/BlocksParser.js"
+import ParserBlock from "../parser/ParserBlock.js"
+import ICSS from "../enums/ICSS.js"
+
+
 /**
  * Class KeyframeRule
  * @class KeyframeRule
  */
-class KeyframeRule {
+class KeyframeRule extends BlocksParser(ParserBlock) {
 
 	/**
 	 * Initialize KeyframeRule
 	 * @param {string} name animation name
-	 * @param {string} blocks
+	 * @param {string} keyframe css blocks
 	 * @returns {KeyframeRule}
 	 */
-	constructor(name, blocks) {
-		[ this.name, this.blocks ] = [ name, blocks ]
-		
+	constructor(name, keyframe) {
+		super()
+
+		this.name = name
+		this.keyframe = keyframe
 		return this
+	}
+
+	set keyframe(values) {
+		let blcks = values.split(ICSS.BLOCK.END);blcks.pop()
+		this.blocks = blcks.map(b => this.block(b.trim()))
 	}
 
 }
