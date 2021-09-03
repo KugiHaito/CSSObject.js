803fb1c6c190d7cb16255a26b10ec21b9aef0fa8
Kugi Haito <kugihaito99@gmail.com>
2021-09-03 00:55:51 -0300

    Update ImportRule to parse where condition

--- CSSObject/rules/ImportRule.js
+++ CSSObject/rules/ImportRule.js
@@ -1,3 +1,7 @@
+import ICSS from "../enums/ICSS.js"
+import Rule from "./Rule.js"
+
+
 /**
  * Class ImportRule
  * @class ImportRule
@@ -11,9 +15,31 @@ class ImportRule {
 	 * @returns {ImportRule}
 	 */
 	constructor(url, where = null) {
-		[ this.url, this.where ] = [ url, where ]
+		this.import = { url, where }
 
 		return this
+	}
+
+	set import({url, where}) {
+		this.url = url
+		if (where != null) {
+			this.where = []
+			where.split(ICSS.COMMA)
+				.map(w => {
+					w.split(/and+(\s+\([^)]*\))?/g)
+						.map(i => i.trim()).filter(i => i != "")
+						.map(wh => {
+							if (wh.includes(ICSS.DOTS)) {
+								let [ prop, value ] = wh.replace(/[\(\)]/g, '')
+									.split(ICSS.DOTS).map(i => i.trim())
+								
+								this.where.push(new Rule(prop, value))
+							} else { this.where.push(wh.trim()) }
+						})
+						
+						return this.where
+				})
+		}
 	}
 
 }
