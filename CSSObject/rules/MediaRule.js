1aa07e7a6c2313588927204ef04a90c14ffc5d2f
Kugi Haito <kugihaito99@gmail.com>
2021-09-02 22:21:41 -0300

    Add IMedia enum and queries parse to MediaRule

--- CSSObject/rules/MediaRule.js
+++ CSSObject/rules/MediaRule.js
@@ -1,4 +1,6 @@
+import Rule from "./Rule.js"
 import ICSS from "../enums/ICSS.js"
+import IMedia from "../enums/IMedia.js"
 import ParserBlock from "../parser/ParserBlock.js"
 import BlocksParser from "../parser/BlocksParser.js"
 
@@ -23,10 +25,44 @@ class MediaRule extends BlocksParser(ParserBlock) {
 	}
 
 	set media({query, blocks}) {
-		this.query = query
+		this.querie = query
 		this.blocks = blocks
 			.split(ICSS.BLOCK.END)
 			.map(b => this.block(b))
+	}
+
+	set querie(value) {
+		this.query = value
+		this.queries = this.query.split(ICSS.COMMA)
+			.map(qrie => {
+				let mediaQuerie = {types: [], rules: [], only: false, not: false}
+
+				qrie.split(/and+(\s+\([^)]*\))?/g)
+					.map(i => i.trim()).filter(i => i != '')
+					.map(r => {
+						let rule = r.trim()
+
+						if (rule.includes(ICSS.DOTS)) {
+							let [ prop, value ] = rule.replace(/[\(\)]/g, '')
+								.split(ICSS.DOTS).map(i => i.trim())
+							
+							mediaQuerie.rules.push(new Rule(prop, value))
+							return
+						}
+
+						IMedia.ONLY_OR_NOT.map(operator => {
+							if (rule.startsWith(operator)) {
+								rule = rule.replace(operator, '').trim()
+								mediaQuerie[operator] = true
+							}
+						})
+
+						if (IMedia.MEDIA_TYPES.includes(rule))
+							mediaQuerie.types.push(rule)
+				})
+
+				return { ...mediaQuerie }
+			})
 	}
 
 }
