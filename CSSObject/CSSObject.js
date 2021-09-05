c0fd189d4b5f2d2f0f3f8292291397d88636bf39
Kugi Haito <kugihaito99@gmail.com>
2021-09-05 02:03:37 -0300

    Add support to external stylesheets, options and more

--- CSSObject/CSSObject.js
+++ CSSObject/CSSObject.js
@@ -1,4 +1,3 @@
-import CSSParser from "./CSSParser.js"
 import Stylesheet from "./parser/Stylesheet.js"
 
 
@@ -6,7 +5,7 @@ import Stylesheet from "./parser/Stylesheet.js"
  * CSSObject Main Class
  * parse yours css files and local stylesheets
  */
- class CSSObject extends CSSParser {
+ class CSSObject {
 
 	/**
 	 * CSSObject constructor
@@ -14,19 +13,91 @@ import Stylesheet from "./parser/Stylesheet.js"
 	 * @returns CSSObject
 	 */
 	constructor(options = {}) {
-		super()
-		
-		this.options = options
-
-		this.stylesheets = Object.values(document.styleSheets)
-			.map(sheet => sheet.ownerNode.innerText)
-			.filter(str => str != "")
-			.map((sheet) => new Stylesheet(sheet))
-		
-		this.stylesheets
-			.map(sheet => this.parse(sheet.css))
+		this.options(options)
+		this.stylesheets = []
 
 		return this
+	}
+
+	/**
+	 * set options to get external stylesheets
+	 * @param {object} options options
+	 * @returns CSSObject
+	 */
+	options(options = {}) {
+		this.__defaultOptions__ = {
+
+			/** 
+			 * load only especified css files, if used `ignore_files` isn't used
+			 * @type string[]
+			 */
+			only_files: (options.only_files || []),
+
+			/** 
+			 * ignore css files
+			 * @type string[]
+			 */
+			ignore_files: (options.ignore_files || []),
+
+			/** 
+			 * load minified css (default `true`)
+			 * @type boolean
+			 */
+			load_min: (options.load_min || true),
+		}
+
+		this._options = { ...this.__defaultOptions__, ...options }
+		return this
+	}
+
+	/**
+	 * get local stylesheets
+	 * @param {object} callback callback function
+	 * @param {boolean} all return all stylesheets in callback
+	 */
+	local(callback, all = false) {
+		let styles = []
+		Object.values(document.styleSheets).forEach(sheet => {
+			if (sheet.ownerNode != null || sheet.ownerNode.childNodes.length > 0)
+				if (sheet.ownerNode.innerText != "") {
+					let style = new Stylesheet(sheet.ownerNode.innerText)
+					all ? styles.push(style) : callback(style)
+				}
+		})
+
+		if (styles.length > 0) callback(styles)
+	}
+
+	/**
+	 * get external css (link)
+	 * @param {object} callback callback
+	 */
+	external(callback) {
+		Object.values(document.styleSheets).forEach(sheet => {
+			if (sheet.ownerNode.nodeName == 'LINK') {
+				this.__external(sheet.href, callback)
+			}
+		})
+	}
+
+	/**
+	 * load external stylesheets
+	 * @param {string} link link to external stylesheet
+	 * @returns reponse (promise)
+	 * @private don't use please
+	 */
+	async __external(link, callback) {
+		let filename = link.split('/').pop()
+		let is_min = filename.endsWith('.min.css')
+		if (!this._options.load_min_css && is_min) return
+		if (this._options.only_files.length == 0 && this._options.ignore_files.includes(filename)) return
+		if (this._options.only_files.length > 0 && !this._options.only_files.includes(filename)) return
+
+		return await (fetch(link))
+			.then(res => res.text())
+			.then(css => callback(new Stylesheet(css, filename)))
+			.catch(err => console.log("Error on load external stylesheet: ", err))
+
 	}
 }
 
--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 import CSSObject from "./CSSObject/CSSObject.js"
 
 
-// get css object
+// to show cssobj stylesheet on page
+let div = document.querySelector('.cssobject')
+const printJSON = (style) => {
+	console.log(style)
+	let cssjson = JSON.stringify(style, null, '    ')
+	let pre = document.createElement('pre'),
+	code = document.createElement('code')
+	code.innerText = cssjson
+	pre.appendChild(code)
+	div.appendChild(pre)
+}
+
+// instance css object
 let cssobj = new CSSObject()
 
-// print on page
-let tag = document.querySelector('.cssobject code')
-tag.innerText = JSON.stringify(cssobj, null, '    ')
+// get local stylesheets
+cssobj.local(style => printJSON(style))
 
-// too shows on console
-console.log(cssobj)
+// get external stylesheets
+cssobj.options({ load_min: false })
+	.external(style => printJSON(style))
