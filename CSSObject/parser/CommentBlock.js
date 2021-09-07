7cacb34a4985cbe06d4361ddf504b7e38c97f1f3
Kugi Haito <kugihaito99@gmail.com>
2021-09-07 00:54:26 -0300

    Change CommentBlock class

--- CSSObject/parser/CommentBlock.js
+++ CSSObject/parser/CommentBlock.js
@@ -1,3 +1,6 @@
+import ICSS from "../enums/ICSS.js"
+
+
 /**
  * Class CommentBlock
  * @class CommentBlock
@@ -8,10 +11,37 @@ class CommentBlock {
 	 * Initialize CommentBlock
 	 * @param {string} block
 	 */
-	constructor(block) {
+	constructor(block, line) {
 		this.block = block
+		this.line = line
 
 		return this
+	}
+
+	/**
+	 * set text comment
+	 * @property text
+	 * @param {string} value
+	 */
+	set block(value) {
+		this.text = value.replace(/[*/]/g, "")
+	}
+
+	/**
+	 * get comment (without spaces)
+	 * @property comment
+	 * @returns string
+	 */
+	get comment() {
+		return this.text.trim()
+	}
+
+	/**
+	 * get comment line with delimeters
+	 * @returns string
+	 */
+	toString() {
+		return `${ICSS.COMMENT.BEGIN + this.text + ICSS.COMMENT.END}`
 	}
 
 }
